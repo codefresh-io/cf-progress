f3c84e9049d1d55e8c7acad173871c13208f28ba
Itai Gendler <itai@codefresh.io>
2016-02-28 15:53:43 +0200

    add initialization

--- tests/basic.unit.spec.js
+++ tests/basic.unit.spec.js
@@ -3,15 +3,89 @@ var expect     = chai.expect;
 var sinon      = require('sinon'); // jshint ignore:line
 var sinonChai  = require('sinon-chai');
 chai.use(sinonChai);
-var cfProgress = require('../lib/index');
+var CfProgress = require('../lib/index');
+var proxyquire = require('proxyquire').noCallThru();
 
+describe('init - initialize CfProgress', function () {
 
-describe('initial', function () {
+    describe('positive', function(){
 
-    it('should test', function () {
-        var progress = new cfProgress();
-        var res = progress.test();
-        expect(res).to.equal(2);
+        it('should succeed when providing both url and secret', function () {
+            var CfProgress = proxyquire('../lib/index', {
+                'firebase': function(){
+                    return {
+                        child: function(){
+                            return this;
+                        },
+                        set: function(){
+                            return this;
+                        },
+                        authWithCustomToken: function(secret, callback){
+                            callback();
+                        }
+                    };
+                }
+            });
+
+            CfProgress.init("url", "secret");
+
+        });
+
     });
+
+    describe('negative', function(){
+
+        it('should fail when not providing firebase url', function (done) {
+            try{
+                CfProgress.init();
+            }
+            catch(e){
+                expect(e.toString()).to.equal("Error: CfProgress requires firebase url");
+                return done();
+            }
+            throw new Error("should have failed");
+        });
+
+        it('should fail when not providing firebase secret', function (done) {
+            try{
+                CfProgress.init("url");
+            }
+            catch(e){
+                expect(e.toString()).to.equal("Error: CfProgress requires firebase secret");
+                return done();
+            }
+            throw new Error("should have failed");
+        });
+
+        it('should fail when authentication aginst firebase fails', function(done){
+
+            var CfProgress = proxyquire('../lib/index', {
+                'firebase': function(){
+                    return {
+                        child: function(){
+                            return this;
+                        },
+                        set: function(){
+                            return this;
+                        },
+                        authWithCustomToken: function(secret, callback){
+                            callback(new Error("error from firebase lib"));
+                        }
+                    };
+                }
+            });
+
+            try{
+                CfProgress.init("url", "firebase");
+            }
+            catch(e){
+                expect(e.toString()).to.equal("Error: CfProgress login to Firebase failed. Caused by: Error: error from firebase lib");
+                return done();
+            }
+            throw new Error("should have failed");
+        });
+
+    });
+
 
 });
